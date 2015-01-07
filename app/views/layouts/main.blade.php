d5e202dc351e5bde7f31cb7c06c3f927a159a11f
Jim Rohacik <sagemvc@gmail.com>
2015-01-07 05:34:51 -0600

    changed branded link to root

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -34,7 +34,7 @@
     		            <span class="icon-bar"></span>
     		            <span class="icon-bar"></span>
     		        </button>
-    		        <a href="#" class="navbar-brand">Laravel Base</a>
+    		        <a href="/" class="navbar-brand">Laravel Base</a>
     		    </div>
     		    
     		    <div class="collapse navbar-collapse" id="main-navbar-collapse-1">
