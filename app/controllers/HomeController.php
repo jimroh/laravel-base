a0a8c72ff1cca1d6f0c4ab9dd72dc314355192f3
Jim Rohacik <jimroh12345@gmail.com>
2015-02-12 12:58:02 -0600

    added login / logout code

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -5,6 +5,8 @@ namespace Controllers;
 use Controllers\BaseController;
 
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class HomeController extends BaseController 
 {
@@ -33,5 +35,67 @@ class HomeController extends BaseController
 				->with('pageTitle', 'Laravel Base')
 				->with('metaDescription', 'This is a base Laravel set up using Twitter Bootstrap 3.0, jQuery and AngularJS.')
 			;
+	}
+
+	/**
+	 * Get action for the login view
+	 *
+	 * @return void
+	 */
+	public function getLogin()
+	{
+		if (Auth::check()) {
+			return
+				Redirect::intended(Auth::user()->landing_page);
+		}
+
+		return 
+			View::make('home.login')
+				->with('pageTitle', 'Login')
+				->with('metaDescription', 'Login Page.')
+				->with('active', 'login')
+			;
+	}
+
+	/**
+	 * Post action for the login view.
+	 *
+	 * @return Redirect
+	 */
+	public function postLogin()
+	{
+		if (Auth::check()) {
+			return
+				Redirect::intended(Auth::user()->landing_page);
+		}
+
+		$data = 
+			Input::only(
+				'username', 
+				'password', 
+				'remember_me'
+			);
+
+		// Try to log in
+		if($this->user->login($data['username'], $data['password'], $data['remember_me'])) {
+			return 
+				Redirect::intended(Auth::user()->landing_page);
+		}
+
+		return 
+			Redirect::back()->with('error', $this->user->getMessages());
+	}
+
+	/**
+	 * Get action to log out the user
+	 *
+	 * @return void
+	 */
+	public function getLogout()
+	{
+		Auth::Logout();
+
+		return 
+			Redirect::to('/');
 	}
 }
